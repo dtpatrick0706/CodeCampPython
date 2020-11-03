086422827630aae6812aa97e829640da75098421
Devin Patrick <dpatrick0706@gmail.com>
2020-11-03 14:11:32 -0700

    initial commit of chapter 11b

--- Python for Everybody/chapter11b.py
+++ Python for Everybody/chapter11b.py
@@ -1,2 +1,7 @@
     ## Regular Expressions: Matching and Exrtracting Data ##
 
+# Matching and Extracting #
+
+# re.search () returns a True/False depending on whether the string matches the regular expression
+# If we actually want the matching string to be extracted, we use re.findall()
+

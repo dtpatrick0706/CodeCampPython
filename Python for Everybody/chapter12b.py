33e12552f000fa02d3e3f828527da9074adc53f7
Devin Patrick <dpatrick0706@gmail.com>
2020-11-04 14:58:28 -0700

    finish of 12b and 12c

---
+++ Python for Everybody/chapter12b.py
@@ -0,0 +1,8 @@
+        ## Networking Protocols ##
+    # Application Protocol #
+
+# Since TCP (and Python) gives us a reliable socket, what do we want to do with the socket? What problem do we want to solve?
+
+    # HTTP - Hypertext Transfer Protocol #
+
+# The dominant Application Layer Protocol on the internet 
